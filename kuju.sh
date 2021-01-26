86815383332300c3c66862d1dc3506e421055b40
MoiColl <moisescollmacia@gmail.com>
2021-01-26 16:44:48 +0100

    change env name for variable

--- kuju.sh
+++ kuju.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #A. Defining variables
-ku_us="XXXXXX"
+ku_user="XXXXXX"
+envname="ENVENV"
 c1=$1
 p1="9999"
 p2="8888"
@@ -14,21 +15,21 @@ if [ "$2" != "kill" ]
 then
 	#B. Step by  step
 	#B.1 Step 1 - Running jupyter notebook on a computing node 
-	if [ "`ssh -J ${ku_us}@ssh-snm-willerslev.science.ku.dk ${ku_us}@${c1} 'tmux ls | egrep '${m1}''`" = "" ]
+	if [ "`ssh -J ${ku_user}@ssh-snm-willerslev.science.ku.dk ${ku_user}@${c1} 'tmux ls | egrep '${m1}''`" = "" ]
 	then
 		echo "1) Starting jupyter notebook at ${c1} in a tmux session named ${m1} with port ${c1}"
-		ssh -J ${ku_us}@ssh-snm-willerslev.science.ku.dk ${ku_us}@${c1} 'tmux new-session -s '${m1}' -d'
-		ssh -J ${ku_us}@ssh-snm-willerslev.science.ku.dk ${ku_us}@${c1} 'tmux send-keys -t '${m1}' "conda activate spacetime" C-m "jupyter lab --no-browser --port='${p1}'" C-m'
+		ssh -J ${ku_user}@ssh-snm-willerslev.science.ku.dk ${ku_user}@${c1} 'tmux new-session -s '${m1}' -d'
+		ssh -J ${ku_user}@ssh-snm-willerslev.science.ku.dk ${ku_user}@${c1} 'tmux send-keys -t '${m1}' "conda activate '${envname}'" C-m "jupyter lab --no-browser --port='${p1}'" C-m'
 	else
 		echo "1) ${c1} already has a tmux session named ${m1}"
 	fi
 
 	#B.1 Step 1 - Running jupyter notebook on a computing node 
-	if [ "`ssh ${ku_us}@ssh-snm-willerslev.science.ku.dk 'tmux ls | egrep '${m2}''`" = "" ]
+	if [ "`ssh ${ku_user}@ssh-snm-willerslev.science.ku.dk 'tmux ls | egrep '${m2}''`" = "" ]
 	then
 		echo "2) Starting a ssh tunnel from Willerslev servers' front-end (port ${p2}) to ${c1} (port ${p1}) in a tmux session named ${m2}"
-		ssh ${ku_us}@ssh-snm-willerslev.science.ku.dk 'tmux new-session -s '${m2}' -d'
-		ssh ${ku_us}@ssh-snm-willerslev.science.ku.dk 'tmux send-keys -t '${m2}' "conda activate spacetime" C-m "ssh '${c1}' -L '${p2}':localhost:'${p1}' -N" C-m'
+		ssh ${ku_user}@ssh-snm-willerslev.science.ku.dk 'tmux new-session -s '${m2}' -d'
+		ssh ${ku_user}@ssh-snm-willerslev.science.ku.dk 'tmux send-keys -t '${m2}' "ssh '${c1}' -L '${p2}':localhost:'${p1}' -N" C-m'
 		                                                                                                                                            
 	else
 		echo "2) Willerslev servers' front-end already has a tmux session named ${m2}"
@@ -39,7 +40,7 @@ then
 	then
 		echo "3) Starting a ssh tunnel from Local computer (port ${p3}) to Willerslev servers' front-end (port ${p2}) in a tmux session named ${m3}"
 		tmux new-session -s ${m3} -d
-		tmux send-keys -t ${m3}  "ssh ${ku_us}@ssh-snm-willerslev.science.ku.dk -L '${p3}':localhost:'${p2}' -N" C-m
+		tmux send-keys -t ${m3}  "ssh ${ku_user}@ssh-snm-willerslev.science.ku.dk -L '${p3}':localhost:'${p2}' -N" C-m
 
 		                                                                                                                                            
 	else
@@ -50,10 +51,11 @@ then
 	echo "4) Open Google Chrome to access jupyter notebook on your local computer"
 	open --new -a "Google Chrome" --args "http://localhost:${p3}/"
 else
+	#C. If killing option, kill the 3 tmux servers
 	echo "Killing tmux server from ${c1}"
-	ssh -J ${ku_us}@ssh-snm-willerslev.science.ku.dk ${ku_us}@${c1} 'tmux kill-server'
+	ssh -J ${ku_user}@ssh-snm-willerslev.science.ku.dk ${ku_user}@${c1} 'tmux kill-server'
 	echo "Killing tmux server from Willerslev fron end"
-	ssh ${ku_us}@ssh-snm-willerslev.science.ku.dk 'tmux kill-server'
+	ssh ${ku_user}@ssh-snm-willerslev.science.ku.dk 'tmux kill-server'
 	echo "Killing tmux server from local computer"
 	tmux kill-server
 fi
